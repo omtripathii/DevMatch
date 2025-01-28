e98370fad47a8a526362d696d9ca6b9a06cdec8b
OM TRIPATHI <tripathi111om@gmail.com>
2025-01-29 03:49:39 +0530

    Testing middlewares

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,19 +9,43 @@ const app = express();
     
 // })
 
-app.use("/home",(req,res,next)=>{
-    console.log('This is my own route handler 1');
-    next();
-},(req,res,next)=>{
-    console.log('This is my own route handler 2');
-    next();
-},(req,res,next)=>{
-    console.log('This is my own route handler 3');
-    next();
-},(req,res)=>{
-    console.log('This is my own route handler 4');
-    res.send('This is my own middleware and i am handling multiple routes with this')
+// app.use("/home",(req,res,next)=>{
+//     console.log('This is my own route handler 1');
+//     next();
+// },(req,res,next)=>{
+//     console.log('This is my own route handler 2');
+//     next();
+// },(req,res,next)=>{
+//     console.log('This is my own route handler 3');
+//     next();
+// },(req,res)=>{
+//     console.log('This is my own route handler 4');
+//     res.send('This is my own middleware and i am handling multiple routes with this')
+// })
+
+
+
+const {adminAuth , userAuth} = require("./middlewares/auth");
+
+app.use("/admin",adminAuth)
+
+app.get("/admin/getAllData",(req,res)=>{
+    console.log('This is admin route and we are getting all data');
+    res.send('Data is fetched successfully')
 })
+app.post("/admin/deleteUser",(req,res)=>{
+    res.send('user is deleted successfully')
+})
+
+app.get("/user/login",(req,res)=>{
+    res.send('User is logged in successfully')
+})
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send(' user Data is fetched successfully')
+})
+
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
---
+++ Backend/src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const adminAuth = (req, res, next) => {
+    console.log("admin is getting authenticated");
+    
+    const token = "xyz"
+    const isValid = token === "xyz"
+    if (!isValid) {
+        res.status(401).send('Unauthorized , You are not an admin')
+    } else {
+        next()
+    }
+}
+const userAuth = (req, res, next) => {
+    console.log("user is getting authenticated");
+    
+    const token = "xyz"
+    const isValid = token === "xyz"
+    if (!isValid) {
+        res.status(401).send('Unauthorized , You are not an user')
+    } else {
+        next()
+    }
+}
+
+module.exports = { adminAuth, userAuth }
