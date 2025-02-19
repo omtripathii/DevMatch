9a07c1ec636ef3764dd0b6dfce8c4e1a621b4877
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-19 10:41:17 +0530

    newAPi

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-
+const User = require("../models/User");
 const USER_SAFE_DATA = "firstName lastName about photoUrl skills";
 
 // Get API to fetch all the received requests for a User
@@ -15,13 +15,12 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
       status: "interested",
     }).populate("fromUserId", USER_SAFE_DATA);
 
+    const newData = connectionRequests.map((item) => item.fromUserId);
     if (!connectionRequests) {
       return res.status(404).json({ message: "No Requests are Pending" });
     }
 
-    res
-      .status(200)
-      .json({ message: "Your Pending Requests: ", connectionRequests });
+    res.status(200).json({ message: "Your Pending Requests: ", newData });
   } catch (error) {
     res.status(400).send("Something went wrong " + error);
   }
@@ -36,19 +35,59 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
     const data = connectionRequests;
     const newData = data.map((item) => {
-      if(item.fromUserId._id.toString() === loggedInUser._id.toString() ){
-        return item.toUserId
+      if (item.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return item.toUserId;
       }
-      return item.fromUserId
-    }
-    );
+      return item.fromUserId;
+    });
     res.status(200).json({ message: "Your Connection are ", newData });
   } catch (error) {
     res.status(400).send("Something went wrong " + error);
   }
 });
 
+// User Feed API
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  /**
+   * Logged In user should not see his profile himself
+     Logged In user should not see the profile he seem to be interested or has ignored
+     Should not see the users who are already his connections
+   */
+  try {
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserList = new Set();
+    connectionRequest.forEach((item) => {
+      hideUserList.add(item.fromUserId.toString());
+      hideUserList.add(item.toUserId.toString());
+    });
+
+    const feedShow = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserList) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).send(feedShow);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = userRouter;
