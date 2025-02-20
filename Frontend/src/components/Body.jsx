d60568f8f88d7952fe5fefcf4e62bc03676c6677
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-20 23:29:10 +0530

    new changes

--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import Navbar from './Navbar'
-import {Outlet} from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import store from "../utils/appStore";
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+      console.log(res.data);
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.error("Something went wrong " + error);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import store from "../utils/appStore";
+import { Link } from "react-router-dom";
 function Navbar() {
   const user = useSelector((store) => store.user);
   return (
     <>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">🧑‍💻DevMatch</a>
+          <Link to="/" className="btn btn-ghost text-xl">
+            🧑‍💻DevMatch{" "}
+          </Link>
         </div>
         <div className="flex gap-2">
           {user && (
@@ -29,10 +32,10 @@ function Navbar() {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to = "/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
