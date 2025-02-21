da89f6600ffa605257197102c7b963703843ac9a
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-21 10:17:06 +0530

    implement fetching and storing user connections and requests; integrate Redux for state management

--- Frontend/src/components/Connections.jsx
+++ Frontend/src/components/Connections.jsx
@@ -1,17 +1,20 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnection } from "../utils/connectionSlice";
 
 const Connections = () => {
   const [error, setError] = useState(null);
+  const dispatch = useDispatch()
+  const connections = useSelector((store)=>store.connections)
   const fetchConnections = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/connections", {
         withCredentials: true,
       });
       console.log(res?.data?.newData);
-
-      
+      dispatch(addConnection(res?.data?.newData))
     } catch (error) {
       setError(error);
     }
--- Frontend/src/components/Requests.jsx
+++ Frontend/src/components/Requests.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests } from "../utils/requestSlice";
 
 const Requests = () => {
-  return (
-    <div>Requests</div>
-  )
-}
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const requests = useSelector((store) => store.requests);
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
+      console.log(res?.data?.newData);
+      dispatch(addRequests(res?.data?.newData));
+    } catch (error) {
+      setError(error);
+    }
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+  return <div>Requests</div>;
+};
 
-export default Requests
+export default Requests;
