adf11a1834eb7cc63e9f176db22e52cd1190476b
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-21 10:25:32 +0530

    remove unused import of React in Requests component

--- Frontend/src/components/Requests.jsx
+++ Frontend/src/components/Requests.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
