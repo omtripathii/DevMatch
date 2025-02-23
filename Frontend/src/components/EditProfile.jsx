40c3eec0a384bdf39360b389b55eac93fdce96f1
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-23 10:55:52 +0530

    deployment

--- Frontend/src/components/EditProfile.jsx
+++ Frontend/src/components/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import UserCard from "./Usercard";
+import UserCard from "./UserCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
